1f6b54f76facb703de3307b657e6334704eed232
Yupi <youngperchik2404@gmail.com>
2025-03-08 20:44:40 +0500

    Almost finished 5 task

--- Program.cs
+++ Program.cs
@@ -1,10 +1,41 @@
 ﻿namespace Howemork4
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            
+            List<Car> cars = new List<Car>();
+
+            cars.Add(new Car("12df34", "Toyata", "Mark II", 1999, "Orange"));
+            cars.Add(new Car("22we344", "BMW", "M5", 2020, "White"));
+            cars.Add(new Car("349clmd79", "Porche", "911", 2024, "Yellow"));
+            cars.Add(new Car("678gjhg47", "Honda", "Civic", 1990, "Blue"));
+            cars.Add(new Car("974gf6", "Lincoln", "Continental", 1970, "Black"));
+
+
+            Console.WriteLine("Добавьте новую машину: ");
+
+            Console.Write("Введите марку: ");
+            string brand = Convert.ToString(Console.Read());
+
+            Console.Write("Введите модель: ");
+            string model = Convert.ToString(Console.Read());
+
+            Console.Write("Введите год выпуска: ");
+            int year = (int)Convert.ToInt64(Console.Read());
+
+            Console.Write("Введите цвет: ");
+            string colour = Convert.ToString(Console.Read());
+
+            Console.Write("Введите вин-номер: ");
+            string vinNum = Convert.ToString(Console.Read());
+
+            cars.Add(new Car(vinNum, brand, model, year, colour));
+
+            foreach (var i in cars)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
--- obj/Debug/net8.0/Howemork4.AssemblyInfo.cs
+++ obj/Debug/net8.0/Howemork4.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Howemork4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06040dd50821f4a5782d84930e981149260f8607")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1324366a4f97ef7b00c24c3bbcc66c5fa59da34")]
 [assembly: System.Reflection.AssemblyProductAttribute("Howemork4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Howemork4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
